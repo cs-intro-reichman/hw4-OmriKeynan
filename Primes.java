054ed87885d7e7a65fcbcd40fe52f88d3283b82b
Omri Keynan <omri.keynan@post.runi.ac.il>
2024-12-02 14:25:25 +0200

    task 2

--- Primes.java
+++ Primes.java
@@ -1,5 +1,35 @@
 public class Primes {
     public static void main(String[] args) {
-        // Replace this statement with your code
+        int n = Integer.parseInt(args[0]);
+        boolean[] arr = new boolean[n + 1];
+        int i = 2;
+        while (i <= n) {
+            arr[i] = true;
+            i++;
+        }
+        int p = 2;
+        while (p * p <= n) {
+            if (arr[p]) {
+                int multiple = p * p;
+                while (multiple <= n) {
+                    arr[multiple] = false;
+                    multiple += p;
+                }
+            }
+            p++;
+        }
+        int primeCount = 0;
+        System.out.println("Prime numbers up to " + n + ":");
+        int j = 2;
+        while (j <= n) {
+            if (arr[j]) {
+                System.out.println(j);
+                primeCount++;
+            }
+            j++;
+        }
+        double percentage = (primeCount * 100.0) / n;
+        System.out.println("There are " + primeCount + " primes between 2 and " + n +
+                           " (" + String.format("%.0f", percentage) + "% are primes)");
     }
 }
